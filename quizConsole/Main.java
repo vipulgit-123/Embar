46d92a315ebd3539c4cbea1481ee963d501f0a5b
vipulAgnihotri <vipul99819@gmail.com>
2026-07-14 12:33:14 +0530

    My 3rd Commit

--- quizConsole/Main.java
+++ quizConsole/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("HEllo");
         System.out.println("Vipul");
+        System.out.println("Agnihotri");
     }
 }
